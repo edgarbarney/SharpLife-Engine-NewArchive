42cbc0a2c96eaa9e38e96ec915335398ac6f5530
Sam <duo929@hotmail.com>
2019-03-19 19:44:49 +0100

    Implement basic worldspawn entity

---
+++ src/SharpLife.Game/Entities/Factories/WorldFactory.cs
@@ -0,0 +1,54 @@
+﻿/***
+*
+*	Copyright (c) 1996-2001, Valve LLC. All rights reserved.
+*	
+*	This product contains software technology licensed from Id 
+*	Software, Inc. ("Id Technology").  Id Technology (c) 1996 Id Software, Inc. 
+*	All Rights Reserved.
+*
+*   This source code contains proprietary and confidential information of
+*   Valve LLC and its suppliers.  Access to this code is restricted to
+*   persons who have executed a written SDK license with Valve.  Any access,
+*   use or distribution of this code by or to any unlicensed person is illegal.
+*
+****/
+
+using SharpLife.Engine.Entities;
+using SharpLife.Engine.Entities.Components;
+using SharpLife.Engine.Entities.Factories;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace SharpLife.Game.Entities.Factories
+{
+    [LinkEntityToFactory(ClassName = "worldspawn")]
+    public sealed class WorldFactory : EntityFactory
+    {
+        protected override void GetComponentTypes(ImmutableHashSet<Type>.Builder types)
+        {
+            types.Add(typeof(Transform));
+            types.Add(typeof(BSPRenderableComponent));
+        }
+
+        public override bool Initialize(EntityCreator creator, Entity entity, IReadOnlyList<KeyValuePair<string, string>> keyValues)
+        {
+            if (!creator.InitializeComponent(entity.GetComponent<Transform>(), keyValues))
+            {
+                return false;
+            }
+
+            var bspRenderable = entity.GetComponent<BSPRenderableComponent>();
+
+            if (creator.Scene.WorldState.MapInfo.Model == null)
+            {
+                creator.Logger.Error("Missing world model for worldspawn. This indicates a serious problem with the engine");
+                return false;
+            }
+
+            bspRenderable.BSPModel = creator.Scene.WorldState.MapInfo.Model;
+
+            return true;
+        }
+    }
+}
